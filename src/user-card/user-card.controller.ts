d4f15193b905f020d823ffad6b5d54e750d402ce
d-o0o-b11 <jimin8830@naver.com>
2023-06-28 11:22:49 +0900

    ✨ - #8 user-card controller, service 수정 완

--- src/user-card/user-card.controller.ts
+++ src/user-card/user-card.controller.ts
@@ -7,12 +7,24 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
+  InternalServerErrorException,
 } from "@nestjs/common";
 import { UserCardService } from "./user-card.service";
 import { CreateUserCardDto } from "./dto/create-user-card.dto";
 import { UpdateUserCardDto } from "./dto/update-user-card.dto";
-import { ApiBody, ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from "@nestjs/swagger";
 import { FindAllUserCard } from "./dto/findAll-card.dto";
+import { JwtAccessAuthGuard } from "src/kakao-oauth/jwt-access.guard";
+import { CtxUser } from "src/kakao-oauth/decorator/auth.decorator";
+import { JWTToken } from "src/kakao-userinfo/dto/jwt-token.dto";
 
 @ApiTags("유저 카드 API")
 @Controller("user-card")
@@ -29,62 +41,127 @@ export class UserCardController {
   @ApiBody({
     type: CreateUserCardDto,
   })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @Post()
-  async createUserCard(@Body() dto: CreateUserCardDto) {
-    return await this.userCardService.createUserCard(dto);
+  async createUserCard(
+    @Body() dto: CreateUserCardDto,
+    @CtxUser() token: JWTToken
+  ) {
+    return await this.userCardService.createUserCard(dto, token.id);
   }
 
   @ApiOperation({
     summary: "유저 위시 카드 제목,내용 수정",
   })
+  @ApiInternalServerErrorResponse({
+    description: "위시 카드 제목, 내용 수정 실패",
+  })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @Patch()
   async updateUserCard(@Body() dto: UpdateUserCardDto) {
-    return await this.userCardService.updateUserCard(dto);
+    try {
+      return await this.userCardService.updateUserCard(dto);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
     summary: "유저 위시 카드 성공 ",
   })
-  @ApiParam({ name: "card_id", example: 1, type: Number })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
+  @ApiParam({
+    name: "card_id",
+    description: "card_id",
+    example: 1,
+    type: Number,
+  })
   @Post("finish/:card_id")
   async finishUserCard(@Param("card_id") card_id: number) {
-    return await this.userCardService.finishUserCard(card_id);
+    try {
+      return await this.userCardService.finishUserCard(card_id);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
     summary: "해당 폴더에 있는 모든 위시 카드 출력",
+    description:
+      "default_folder_id , user_folder_id 하나의 값만 넣어서 주시면 돼요! 둘 다 넣으시면 오류 발생!!! ",
   })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @Get()
-  async getAllCardOfFolder(@Query() dto: FindAllUserCard) {
-    return await this.userCardService.findUserCardOfFolder(dto);
+  async getAllCardOfFolder(
+    @Query() dto: FindAllUserCard,
+    @CtxUser() token: JWTToken
+  ) {
+    try {
+      return await this.userCardService.findUserCardOfFolder(dto, token.id);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
     summary: "유저 위시 카드 삭제 ",
   })
+  @ApiInternalServerErrorResponse({
+    description: "위시 카드 삭제 실패",
+  })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @ApiParam({ name: "card_id", example: 1, type: Number })
   @Delete("finish/:card_id")
   async deleteUserCard(@Param("card_id") card_id: number) {
-    return await this.userCardService.deleteUserCard(card_id);
+    try {
+      return await this.userCardService.deleteUserCard(card_id);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
     summary: "유저의 모든 위시 카드 출력 ",
+    description: "달성 여부는 finishDay 변수에 표시 (false , 달성한 날짜)",
   })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @Get("all")
-  async findAllUserCards() {
-    const user_id = 3;
-    return await this.userCardService.findAllUserCard(user_id);
+  async findAllUserCards(@CtxUser() token: JWTToken) {
+    try {
+      return await this.userCardService.findAllUserCard(token.id);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
     summary: "검색 기능으로 유저 위시 카드 출력",
   })
-  @Get("search")
-  async searchUserCards() {
-    const user_id = 3;
-    const search_word = "22";
-    return await this.userCardService.searchAllUserCard(user_id, search_word);
+  @ApiParam({
+    name: "search_word",
+    example: "테스트",
+  })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
+  @Get("search/:search_word")
+  async searchUserCards(
+    @Param("search_word") search_word: string,
+    @CtxUser() token: JWTToken
+  ) {
+    try {
+      return await this.userCardService.searchAllUserCard(
+        token.id,
+        search_word
+      );
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 
   @ApiOperation({
@@ -92,8 +169,14 @@ export class UserCardController {
     description:
       "total_count -> 전체 이룬 위시 갯수 , folder_of_count -> 각 폴더별 이룬 위시 개수 ",
   })
+  @ApiBearerAuth("access-token")
+  @UseGuards(JwtAccessAuthGuard)
   @Get("card_count")
-  async finishUsreCardCount() {
-    return await this.userCardService.finishUserCardCount(3);
+  async finishUsreCardCount(@CtxUser() token: JWTToken) {
+    try {
+      return await this.userCardService.finishUserCardCount(token.id);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
   }
 }
