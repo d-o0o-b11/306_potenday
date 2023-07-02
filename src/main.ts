535ea6b63db37d0cef94faa06235e7d10066fe9f
d-o0o-b11 <jimin8830@naver.com>
2023-07-02 13:03:14 +0900

    🔥

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,27 @@ import { AppModule } from "./app.module";
 import { SetUpConfig } from "./setup.config";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { corsMiddleware } from "./user-folder/cors-middleware";
+import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.use(corsMiddleware)
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "https://accounts.kakao.com",
+        "https://kauth.kakao.com",
+        "https://potenday-project.github.io",
+        "https://potenday-project.github.io/Wishu/",
+        "https://potenday-project.github.io/Wishu",
+        "https://potenday-project.github.io/Wishu/*",
+      ],
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+      allowedHeaders: "Content-Type, Accept, Authorization",
+      credentials: true,
+    })
+  );
 
   const configService = new SetUpConfig(app);
   await configService.setUp();
