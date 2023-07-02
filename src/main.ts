4dace91d2ca84f609f9c9cccbadc6d469aaa4748
d-o0o-b11 <jimin8830@naver.com>
2023-07-02 09:58:29 +0900

    🔥

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SetUpConfig } from "./setup.config";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import * as cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(cors());
 
   const configService = new SetUpConfig(app);
   await configService.setUp();
